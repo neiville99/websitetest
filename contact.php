7c5d7613343e30fe249673cb8a920856738393eb
neiville99 <neivillem28@gmail.com>
2024-08-28 13:34:04 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "neivillem28@gmail.com"; // Replace with your email address
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Your message has been sent successfully!";
+    } else {
+        echo "There was a problem sending your message. Please try again.";
+    }
+}
+?>
